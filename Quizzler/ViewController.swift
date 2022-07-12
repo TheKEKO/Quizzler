8285ef0dc8c7e0c62be5823ed13ed07064aad0b2
Aleksandr F <a7214370@gmail.com>
2022-07-12 21:56:04 +0300

    Setup project

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
-
+    @IBAction func answerBottonPressed(_ sender: UIButton) {
+    }
 }
 
